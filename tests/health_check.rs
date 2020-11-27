d919e704ef16594f2f8e7dc1522f358caaeaae88
oscar bernardini <bernardini687@gmail.com>
2020-11-27 17:46:24 +0100

    feature: initial subscriptions endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,19 @@
 use std::net::TcpListener;
 
+/// Start an instance of the application and
+/// return its address (i.e. `"http://localhost:XXXX"`).
+fn spawn_app() -> String {
+    // port 0 is special one at the OS level:
+    // binding to it will trigger a scan for an available port which will then be bound to the application
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
+
+    let port = listener.local_addr().unwrap().port(); // unwrap the port before giving ownership of `listener` to `run`
+    let server = zero2prod::run(listener).expect("Failed to listen to the address.");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
 // testing equivalent of `actix_rt::main`.
 // you can inspect what code gets generated using:
 // `cargo expand --test health_check`
@@ -18,14 +32,46 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    // port 0 is special one at the OS level:
-    // binding to it will trigger a scan for an available port which will then be bound to the application
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
+#[actix_rt::test]
+async fn subscribtions_returns_200_on_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let params = [("email", "an@ton.io"), ("name", "antonio")];
 
-    let port = listener.local_addr().unwrap().port(); // unwrap the port before giving ownership of `listener` to `run`
-    let server = zero2prod::run(listener).expect("Failed to listen to the address.");
-    let _ = tokio::spawn(server);
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .form(&params)
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
-    format!("http://127.0.0.1:{}", port)
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribtions_returns_400_on_invalid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let table_driven_test_cases = vec![
+        ("email=mr_bill%40example.com", "missing the name"),
+        ("name=mr%20bill", "missing the email"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, failed_test_message) in table_driven_test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            failed_test_message
+        );
+    }
 }
